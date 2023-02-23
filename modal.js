f1ff3b602625d65b0a5f8165b9ad257892ce6a8d
emrey2 <emrewp2@gmail.com>
2023-02-23 21:50:28 +0300

    init commit

---
+++ modal.js
@@ -0,0 +1,9 @@
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".container").style.display = "none";
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".container").style.display = "block";
+  }, 2000);
+});
