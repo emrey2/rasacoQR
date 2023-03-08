d06d9d97a0d2d0d0d1345b44d751ab01397108c6
emrey2 <emrewp2@gmail.com>
2023-03-08 16:28:23 +0300

    compresso

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ document.querySelector(".close").addEventListener("click", function () {
   document.querySelector(".container").style.display = "none";
 });
 
-window.addEventListener("load", function () {
+window.addEventListener("load", async function () {
   setTimeout(function open(event) {
     document.querySelector(".container").style.display = "block";
   }, 2000);
